a763285a42e05bc631ef4e9f398930c8e0cba36a
djluka <luka.djordjevic7017@gmail.com>
2022-11-08 13:39:54 +0100

    hi

--- kontrola/script.js
+++ kontrola/script.js
@@ -1,13 +1,5 @@
-// const rasvetaButton = document.querySelector("#rasveta").addEventListener('click', function(e) {
-//     var box = document.querySelector(".box")
-//     box.classList.remove("display-none");
-//     box.classList.remove("position");
-//     console.log(box)
+const rasvetaButton = document.querySelector("#rasveta").addEventListener('click', function(e) {
+})
 
-// })
-// const grejanjeButton = document.querySelector("#grejanje").addEventListener('click', function(e) {
-//     var box = document.querySelector(".box")
-//     box.classList.remove("display-none");
-//     box.classList.add("position");
-//     console.log(box)
-// })
+const grejanjeButton = document.querySelector("#grejanje").addEventListener('click', function(e) {
+})

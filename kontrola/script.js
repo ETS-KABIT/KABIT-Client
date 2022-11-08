178a5431f63eb845b041e59e97f31b45260dcdaf
djluka <luka.djordjevic7017@gmail.com>
2022-11-08 17:53:58 +0100

    HI

--- kontrola/script.js
+++ kontrola/script.js
@@ -1,5 +1,64 @@
-const rasvetaButton = document.querySelector("#rasveta").addEventListener('click', function(e) {
-})
+    let checkStatus = true;
+        const dropdownnButton = document.querySelector("#drpdwn-btn").addEventListener('click', function(e) {
+            var navigation = document.querySelector("nav")
+            var img = document.querySelector("#image")
+            if(checkStatus)
+            {   
+                navigation.classList.remove("reverse-slide")
+                navigation.classList.add("slide")
+                img.setAttribute(
+                    'src',
+                    '/kontrola/assets/up-arrow.png',
+                )
+                checkStatus = !checkStatus;
 
-const grejanjeButton = document.querySelector("#grejanje").addEventListener('click', function(e) {
-})
+            }
+            else if(!checkStatus)
+            {
+                navigation.classList.remove("slide")
+                navigation.classList.add("reverse-slide")
+                img.setAttribute(
+                    'src',
+                    '/kontrola/assets/down-arrow.png',
+                );
+                checkStatus = !checkStatus;
+            }
+
+
+    })
+    const rasveta = document.querySelector("#rasveta").addEventListener('click', function(e) {
+        var navigation = document.querySelector("nav")
+        var img = document.querySelector("#image")
+        console.log(this)
+        navigation.classList.remove("slide")
+        navigation.classList.add("reverse-slide")
+        img.setAttribute(
+            'src',
+            '/kontrola/assets/down-arrow.png',
+        );
+        checkStatus = !checkStatus;    
+    })
+    const grejanje = document.querySelector("#grejanje").addEventListener('click', function(e) {
+        var navigation = document.querySelector("nav")
+        var img = document.querySelector("#image")
+        console.log(this)
+        navigation.classList.remove("slide")
+        navigation.classList.add("reverse-slide")
+        img.setAttribute(
+            'src',
+            '/kontrola/assets/down-arrow.png',
+        );
+        checkStatus = !checkStatus;
+    })
+    const susara = document.querySelector("#susara").addEventListener('click', function(e) {
+        var navigation = document.querySelector("nav")
+        var img = document.querySelector("#image")
+        console.log(this)
+        navigation.classList.remove("slide")
+        navigation.classList.add("reverse-slide")
+        img.setAttribute(
+            'src',
+            '/kontrola/assets/down-arrow.png',
+        );
+        checkStatus = !checkStatus;
+    })

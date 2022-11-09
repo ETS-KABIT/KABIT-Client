05d2db53bbf69b47ddb94fc5d4139f9b16346339
djluka <luka.djordjevic7017@gmail.com>
2022-11-09 14:29:11 +0100

    rasveta

--- kontrola/script.js
+++ kontrola/script.js
@@ -1,29 +1,25 @@
     let checkStatus = true;
-        const dropdownnButton = document.querySelector("#drpdwn-btn").addEventListener('click', function(e) {
-            var navigation = document.querySelector("nav")
-            var img = document.querySelector("#image")
-            if(checkStatus)
-            {   
-                navigation.classList.remove("reverse-slide")
-                navigation.classList.add("slide")
-                img.setAttribute(
-                    'src',
-                    '/kontrola/assets/up-arrow.png',
-                )
-                checkStatus = !checkStatus;
+    const dropdownnButton = document.querySelector("#drpdwn-btn").addEventListener('click', function(e) {
+        var navigation = document.querySelector("nav")
+        var img = document.querySelector("#image")
+        if (checkStatus) {
+            navigation.classList.remove("reverse-slide")
+            navigation.classList.add("slide")
+            img.setAttribute(
+                'src',
+                '/kontrola/assets/up-arrow.png',
+            )
+            checkStatus = !checkStatus;
 
-            }
-            else if(!checkStatus)
-            {
-                navigation.classList.remove("slide")
-                navigation.classList.add("reverse-slide")
-                img.setAttribute(
-                    'src',
-                    '/kontrola/assets/down-arrow.png',
-                );
-                checkStatus = !checkStatus;
-            }
-
+        } else if (!checkStatus) {
+            navigation.classList.remove("slide")
+            navigation.classList.add("reverse-slide")
+            img.setAttribute(
+                'src',
+                '/kontrola/assets/down-arrow.png',
+            );
+            checkStatus = !checkStatus;
+        }
 
     })
     const rasveta = document.querySelector("#rasveta").addEventListener('click', function(e) {
@@ -36,7 +32,7 @@
             'src',
             '/kontrola/assets/down-arrow.png',
         );
-        checkStatus = !checkStatus;    
+        checkStatus = !checkStatus;
     })
     const grejanje = document.querySelector("#grejanje").addEventListener('click', function(e) {
         var navigation = document.querySelector("nav")
@@ -61,4 +57,31 @@
             '/kontrola/assets/down-arrow.png',
         );
         checkStatus = !checkStatus;
+    })
+
+    //input or change
+    const dnevnaSobaSlide = document.querySelector("#dnevna-soba-slide").addEventListener("input", function(e) {
+        var dnevnaSoba = document.querySelector("#dnevna-soba")
+        dnevnaSoba.innerHTML = this.value
+    })
+    const kupatiloSlide = document.querySelector("#kupatilo-slide").addEventListener("input", function(e) {
+        var kupatilo = document.querySelector("#kupatilo")
+        kupatilo.innerHTML = this.value
+    })
+    const hodnikSlide = document.querySelector("#hodnik-slide").addEventListener("input", function(e) {
+        var hodnik = document.querySelector("#hodnik")
+        hodnik.innerHTML = this.value
+    })
+    const stepeniceSlide = document.querySelector("#stepenice-slide").addEventListener("input", function(e) {
+        var stepenice = document.querySelector("#stepenice")
+        stepenice.innerHTML = this.value
+    })
+
+    const soba1Slide = document.querySelector("#soba1-slide").addEventListener("input", function(e) {
+        var soba1 = document.querySelector("#soba1")
+        soba1.innerHTML = this.value
+    })
+    const soba2Slide = document.querySelector("#soba2-slide").addEventListener("input", function(e) {
+        var soba2 = document.querySelector("#soba2")
+        soba2.innerHTML = this.value
     })

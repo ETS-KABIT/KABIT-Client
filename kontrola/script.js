cb37f7e9d0694ce9906865f485679d6fd2b446d0
123stefan <idegas68@gmail.com>
2022-11-22 18:51:23 +0100

    stefan

--- kontrola/script.js
+++ kontrola/script.js
@@ -16,3 +16,26 @@ function run() {
     xhr.send();
 }
 run();
+
+// var input = document.querySelectorAll('input[type ="range"]');
+// for (var i = 0; i < input.length; i++) {
+//     input[i].addEventListener("change", function(e) {
+//         console.log(this.value)
+//     })
+// }
+
+let xhr = new XMLHttpRequest();
+xhr.open("POST", "/kontrola/dnevna-soba");
+xhr.setRequestHeader("Accept", "text");
+xhr.setRequestHeader("Content-Type", "text");
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+    }
+};
+
+let data = "hello";
+
+xhr.send(data);
--- kontrola/style.js
+++ kontrola/style.js
@@ -52,9 +52,9 @@
                 input[i].addEventListener("change", function(e) {
                     var paragraph = this.parentElement.previousElementSibling
                     var form = this.parentElement
-                    console.log(form)
+                        // console.log(form)
                     paragraph.innerHTML = this.value + "%"
-                    form.submit()
+                        // form.submit()
                 })
             }
             checkStatus = !checkStatus;

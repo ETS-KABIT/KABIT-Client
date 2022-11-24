c06cc89003e3cc4c3c7d1430696ed7f4ee3c4f0c
123stefan <idegas68@gmail.com>
2022-11-24 02:09:45 +0100

    pregled

--- pregled/script.js
+++ pregled/script.js
@@ -1,65 +1,127 @@
 // rasveta
-function runRasveta() {
+function runRasvetaDnevnaSoba() {
     var xhr = new XMLHttpRequest();
-    var url = '/request/nadzor/rasveta';
+    var url = '/request/rasveta/dnevnaSoba';
     xhr.open("GET", url, true);
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText);
-            //send complete JSON 
             var DnevnaSoba = document.querySelector("#dnevna-soba-intenzitet")
-            var Kupatilo = document.querySelector("#kupatilo-intenzitet")
-            var Hodnik = document.querySelector("#hodnik-intenzitet")
-            var Stepenice = document.querySelector("#stepenice-intenzitet")
-            var Soba1 = document.querySelector("#soba-1-intenzitet")
-            var Soba2 = document.querySelector("#soba-2-intenzitet")
-
+            DnevnaSoba.innerHTML = this.responseText
         }
-
     }
     xhr.send();
 }
-runRasveta()
-setInterval(runRasveta, 3000)
-    //GREJANJE prizemlje tren temp1
-function runPrizemlje() {
-    // Creating Our XMLHttpRequest object 
+
+function runRasvetaKupatilo() {
     var xhr = new XMLHttpRequest();
-    // Making our connection  
+    var url = '/request/rasveta/kupatilo';
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            var Kupatilo = document.querySelector("#kupatilo-intenzitet")
+            Kupatilo.innerHTML = this.responseText
+        }
+    }
+    xhr.send();
+}
+
+function runRasvetaHodnik() {
+    var xhr = new XMLHttpRequest();
+    var url = '/request/rasveta/hodnik';
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            var Hodnik = document.querySelector("#hodnik-intenzitet")
+            Hodnik.innerHTML = this.responseText
+        }
+    }
+    xhr.send();
+}
+
+function runRasvetaStepenice() {
+    var xhr = new XMLHttpRequest();
+    var url = '/request/rasveta/stepenice';
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            var Stepenice = document.querySelector("#stepenice-intenzitet")
+            Stepenice.innerHTML = this.responseText
+        }
+    }
+    xhr.send();
+}
+
+function runRasvetaSoba1() {
+    var xhr = new XMLHttpRequest();
+    var url = '/request/rasveta/soba1';
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            var Soba1 = document.querySelector("#soba-1-intenzitet")
+            Soba1.innerHTML = this.responseText
+        }
+    }
+    xhr.send();
+}
+
+function runRasvetaSoba2() {
+    var xhr = new XMLHttpRequest();
+    var url = '/request/rasveta/soba2';
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            var Soba2 = document.querySelector("#soba-2-intenzitet")
+            Soba2.innerHTML = this.responseText
+        }
+    }
+    xhr.send();
+}
+runRasvetaDnevnaSoba()
+runRasvetaKupatilo()
+runRasvetaHodnik()
+runRasvetaStepenice()
+runRasvetaSoba1()
+runRasvetaSoba2()
+setInterval(runRasvetaDnevnaSoba, 3000)
+setInterval(runRasvetaKupatilo, 3000)
+setInterval(runRasvetaHodnik, 3000)
+setInterval(runRasvetaStepenice, 3000)
+setInterval(runRasvetaSoba1, 3000)
+setInterval(runRasvetaSoba2, 3000)
+
+function runPrizemlje() {
+    var xhr = new XMLHttpRequest();
     var url = '/request/temp/prizemlje';
     xhr.open("GET", url, true);
 
-    // function execute after request is successful 
     xhr.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                var trenTempPrizemlje = document.querySelector("#prizemlje-temp")
-                console.log(this.responseText);
-            }
+        if (this.readyState == 4 && this.status == 200) {
+            var trenTempPrizemlje = document.querySelector("#prizemlje-temp")
+            trenTempPrizemlje.innerHTML = this.responseText;
         }
-        // Sending our request 
+    }
     xhr.send();
 }
-runPrizemlje()
-    //GREJANJE prvi sprat tren temp1
-function runPrviSprat() {
-    // Creating Our XMLHttpRequest object 
-    var xhr = new XMLHttpRequest();
 
-    // Making our connection  
+function runPrviSprat() {
+    var xhr = new XMLHttpRequest();
     var url = '/request/temp/prvi-sprat';
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var trenTempPrviSprat = document.querySelector("#trenutna-temperatura-prvi-sprat")
-            console.log(this.responseText);
-
+            trenTempPrviSprat.innerHTML = this.responseText
         }
     }
     xhr.send();
 }
-
-runPrviSprat()
 
 function runSpoljasnja() {
     var xhr = new XMLHttpRequest();
@@ -68,12 +130,13 @@ function runSpoljasnja() {
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var trenTempSpolja = document.querySelector("#spoljasnja-temp")
-            console.log(this.responseText);
-
+            trenTempSpolja.innerHTML = this.responseText
         }
     }
     xhr.send();
 }
+runPrizemlje()
+runPrviSprat()
 runSpoljasnja()
 setInterval(runPrizemlje, 10000)
 setInterval(runPrviSprat, 10000)
@@ -88,51 +151,44 @@ function runVentilacijaProzor1() {
         if (this.readyState == 4 && this.status == 200) {
             var prozor1 = document.querySelector("#prozor1-state")
             prozor1.innerHTML = this.responseText
-            console.log(this.responseText);
-
         }
     }
     xhr.send();
 }
-runVentilacijaProzor1()
 
 function runVentilacijaProzor2() {
     var xhr = new XMLHttpRequest();
-    var url = '/request/ventilacija/prozor1';
+    var url = '/request/ventilacija/prozor2';
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var prozor2 = document.querySelector("#prozor2-state")
             prozor2.innerHTML = this.responseText
-            console.log(this.responseText);
-
         }
     }
     xhr.send();
 }
+runVentilacijaProzor1()
 runVentilacijaProzor2()
 setInterval(runVentilacijaProzor1, 3000)
 setInterval(runVentilacijaProzor2, 3000)
 
 function runSusaraTemp() {
     var xhr = new XMLHttpRequest();
-    var url = '/request/susara/temp';
+    var url = '/request/temp/susara';
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var temp = document.querySelector("#susara-temp")
             temp.innerHTML = this.responseText
-            console.log(this.responseText);
-
         }
     }
     xhr.send();
 }
-runSusaraTemp()
 
 function runSusaraHumidity() {
     var xhr = new XMLHttpRequest();
-    var url = '/request/susara/humidity';
+    var url = '/request/humidity/susara';
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -142,6 +198,8 @@ function runSusaraHumidity() {
         }
     }
     xhr.send();
-    w
 }
 runSusaraHumidity()
+runSusaraTemp()
+setInterval(runSusaraHumidity, 10000)
+setInterval(runSusaraTemp, 10000)
